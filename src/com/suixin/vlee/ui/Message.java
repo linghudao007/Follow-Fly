7d40baafa3601c8fe06fe5520d26ee6b7bdf9ce1
Administrator <Administrator@PC-201604142235>
2016-06-02 14:43:12 +0800

    组员李飞上传的项目内容

---
+++ src/com/suixin/vlee/ui/Message.java
@@ -0,0 +1,19 @@
+package com.suixin.vlee.ui;
+
+public class Message {
+
+	private int type;//指定是哪种类�?
+	private String value;//�?
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
