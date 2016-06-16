872e0ab47c23216910f836814fcca9b6b5d81f87
Administrator <Administrator@USER-20160503IY>
2016-06-16 08:34:22 +0800

    qijianghao

---
+++ src/com/suixin/vq/ui/CuttondActivity.java
@@ -0,0 +1,15 @@
+package com.suixin.vq.ui;
+
+import com.suixin.vy.ui.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CuttondActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.item_3_vqmain);
+	}
+}
