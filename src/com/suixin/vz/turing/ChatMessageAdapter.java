0fc5fbcc929a3278cd73975de08d6fb531c37c79
linghudao007 <linghudaoZZY@outlook.com>
2016-06-29 10:17:44 +0800

    图灵机器人 第一版

---
+++ src/com/suixin/vz/turing/ChatMessageAdapter.java
@@ -0,0 +1,103 @@
+package com.suixin.vz.turing;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import com.suixin.vy.ui.R;
+import com.suixin.vz.bean.ChatMessage;
+import com.suixin.vz.bean.ChatMessage.Type;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class ChatMessageAdapter extends BaseAdapter
+{
+	private LayoutInflater mInflater;
+	private List<ChatMessage> mDatas;
+
+	public ChatMessageAdapter(Context context, List<ChatMessage> mDatas)
+	{
+		mInflater = LayoutInflater.from(context);
+		this.mDatas = mDatas;
+	}
+
+	public int getCount()
+	{
+		return mDatas.size();
+	}
+
+	public Object getItem(int position)
+	{
+		return mDatas.get(position);
+	}
+
+	public long getItemId(int position)
+	{
+		return position;
+	}
+
+	public int getItemViewType(int position)
+	{
+		ChatMessage chatMessage = mDatas.get(position);
+		if (chatMessage.getType() == Type.INCOMING)
+		{
+			return 0;
+		}
+		return 1;
+	}
+
+	@Override
+	public int getViewTypeCount()
+	{
+		return 2;
+	}
+
+	public View getView(int position, View convertView, ViewGroup parent)
+	{
+		ChatMessage chatMessage = mDatas.get(position);
+		ViewHolder viewHolder = null;
+		if (convertView == null)
+		{
+			// ͨ��ItemType���ò�ͬ�Ĳ���
+			if (getItemViewType(position) == 0)
+			{
+				convertView = mInflater.inflate(R.layout.item_from_msg, parent,
+						false);
+				viewHolder = new ViewHolder();
+				viewHolder.mDate = (TextView) convertView
+						.findViewById(R.id.id_form_msg_date);
+				viewHolder.mMsg = (TextView) convertView
+						.findViewById(R.id.id_from_msg_info);
+			} else
+			{
+				convertView = mInflater.inflate(R.layout.item_to_msg, parent,
+						false);
+				viewHolder = new ViewHolder();
+				viewHolder.mDate = (TextView) convertView
+						.findViewById(R.id.id_to_msg_date);
+				viewHolder.mMsg = (TextView) convertView
+						.findViewById(R.id.id_to_msg_info);
+			}
+			convertView.setTag(viewHolder);
+		} else
+		{
+			viewHolder = (ViewHolder) convertView.getTag();
+		}
+		// ��������
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		viewHolder.mDate.setText(df.format(chatMessage.getDate()));
+		viewHolder.mMsg.setText(chatMessage.getMsg());
+		return convertView;
+	}
+
+	private final class ViewHolder
+	{
+		TextView mDate;
+		TextView mMsg;
+	}
+
+}
---
+++ src/com/suixin/vz/turing/FragmentLoging.java
@@ -0,0 +1,118 @@
+package com.suixin.vz.turing;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.suixin.vy.ui.R;
+import com.suixin.vz.bean.ChatMessage;
+import com.suixin.vz.bean.ChatMessage.Type;
+import com.suixin.vz.utils.HttpUtils;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class FragmentLoging extends Fragment {
+
+    private ListView mMsgs;
+
+    private ChatMessageAdapter mAdapter;
+
+    private List<ChatMessage> mDatas;
+
+    private EditText mInputMsg;
+
+    private Button mSendMsg;
+    
+    private Activity activity;
+
+    private View view;
+    public void onAttach(android.app.Activity activity) {
+        this.activity = activity;
+        super.onAttach(activity);
+    }
+
+    
+    private Handler mHandler = new Handler() {
+        public void handleMessage(android.os.Message msg) {
+            ChatMessage fromMessge = (ChatMessage) msg.obj;
+            mDatas.add(fromMessge);
+            mAdapter.notifyDataSetChanged();
+            mMsgs.setSelection(mDatas.size() - 1);
+        };
+
+    };
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_im,
+                container, false);
+        this.view = view;
+     // 实例化发现中的控件
+        initDatas();
+        initListener();
+        initLv_find(inflater, container);
+        return view;
+    }
+
+    private void initLv_find(LayoutInflater inflater, ViewGroup container) {
+        mMsgs = (ListView) view.findViewById(R.id.id_listview_msgs);
+        mInputMsg = (EditText) view.findViewById(R.id.id_input_msg);
+        mSendMsg = (Button) view.findViewById(R.id.id_send_msg);
+    }
+
+    private void initListener() {
+        mSendMsg.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                final String toMsg = mInputMsg.getText().toString();
+                if (TextUtils.isEmpty(toMsg)) {
+                    Toast.makeText(activity, "输入不能为空",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                ChatMessage toMessage = new ChatMessage();
+                toMessage.setDate(new Date());
+                toMessage.setMsg(toMsg);
+                toMessage.setType(Type.OUTCOMING);
+                mDatas.add(toMessage);
+                mAdapter.notifyDataSetChanged();
+                mMsgs.setSelection(mDatas.size() - 1);
+
+                mInputMsg.setText("");
+
+                new Thread() {
+                    public void run() {
+                        ChatMessage fromMessage = HttpUtils.sendMessage(toMsg);
+                        Message m = Message.obtain();
+                        m.obj = fromMessage;
+                        mHandler.sendMessage(m);
+                    };
+                }.start();
+
+            }
+        });
+    }
+
+    private void initDatas() {
+        mDatas = new ArrayList<ChatMessage>();
+        mDatas.add(new ChatMessage("您好", Type.INCOMING, new Date()));
+        mAdapter = new ChatMessageAdapter(activity, mDatas);
+        mMsgs.setAdapter(mAdapter);
+    }
+
+}
